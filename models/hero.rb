41d836de9b52410accfe1943a082199d7363ff92
Franklin Webber <franklin.webber@gmail.com>
2013-04-12 10:02:59 -0600

    Animation

--- models/hero.rb
+++ models/hero.rb
@@ -1,8 +1,13 @@
 class Hero < Metro::UI::Sprite
   property :image, path: "hero.png"
 
+  property :walking, type: :animation, path: "hero-walking.png",
+    dimensions: Dimensions.of(128,128), time_per_image: 33
+
   property :turn_amount, default: 80
   property :velocity, default: 4000
+
+  property :scale, default: "1.0,1.0"
 
   property :position
 
@@ -21,7 +26,7 @@ class Hero < Metro::UI::Sprite
 
   def shape
     @shape ||= begin
-      poly_array = [ CP::Vec2.new(-28.0, -28.0), CP::Vec2.new(-28.0, 28.0), CP::Vec2.new(28.0, 28.0), CP::Vec2.new(28.0, -28.0)]
+      poly_array = [ CP::Vec2.new(-48.0, -48.0), CP::Vec2.new(-48.0, 48.0), CP::Vec2.new(48.0, 48.0), CP::Vec2.new(48.0, -48.0)]
       new_shape = CP::Shape::Poly.new(body,poly_array, CP::Vec2::ZERO)
       new_shape.collision_type = :hero
       new_shape.e = 0.0
@@ -32,10 +37,22 @@ class Hero < Metro::UI::Sprite
 
   event :on_hold, KbLeft, GpLeft do
     body.apply_impulse(CP::Vec2.new(-40,0),CP::Vec2.new(0.0, 0.0))
+    # flip left
+    start_walking
   end
 
   event :on_hold, KbRight, GpRight do
     body.apply_impulse(CP::Vec2.new(40,0),CP::Vec2.new(0.0, 0.0))
+    # flip left
+    start_walking
+  end
+
+  event :on_up, KbRight, GpRight do
+    stop_walking
+  end
+
+  event :on_up, KbLeft, GpLeft do
+    stop_walking
   end
 
   event :on_hold, KbDown, GpDown do
@@ -43,22 +60,44 @@ class Hero < Metro::UI::Sprite
 
   event :on_up, KbSpace do
     body.apply_impulse(CP::Vec2.new(0,-2000),CP::Vec2.new(0.0, 0.0))
+    # change to animated jumping
   end
 
   def show
     body.p = CP::Vec2.new(x,y)
   end
 
+  def current_image
+    if walking?
+      walking.image
+    else
+      image
+    end
+  end
+
+  def start_walking
+    @walking = true
+  end
+
+  def stop_walking
+    @walking = false
+  end
+
+  def walking?
+    @walking
+  end
+
   def draw
-    # puts body.v.to_s
-    # dim = Dimensions.of(shape.bb.r - shape.bb.l,shape.bb.b - shape.bb.t)
-    # border = create "metro::ui::border", position: Point.at(shape.bb.l,shape.bb.t), dimensions: dim
-    # border.draw
     dangle = body.a.to_degrees
 
-    hero_x = body.p.x - scene.viewport.left
-    hero_y = body.p.y - scene.viewport.top
-    image.draw_rot(hero_x,hero_y,z_order,dangle)
+    hero_x = body.p.x - scene.tile_map.viewport.left
+    hero_y = body.p.y - scene.tile_map.viewport.top
+    current_image.draw_rot(hero_x,hero_y,z_order,dangle)
+
+    # dim = Dimensions.of(shape.bb.r - shape.bb.l,shape.bb.b - shape.bb.t)
+    # border = create "metro::ui::border", position: Point.at(hero_x - 48,shape.bb.t), dimensions: dim
+    # border.draw
+
   end
 
 end
--- scenes/first_scene.rb
+++ scenes/first_scene.rb
@@ -1,19 +1,13 @@
 class FirstScene < GameScene
   draw :fps, model: "metro::ui::fps", placement: 'bottom_right'
 
-  # draw :rock, position: (Game.center + Point.at(0,200))
+  # BUG: https://github.com/burtlo/metro/issues/4
+  # draw :hero, position: Game.center
+  draw :hero, position: "400,300"
 
-  draw :hero, position: Game.center
-
-  draw :map, model: "metro::ui::tile_map",
-    file: "level.tmx", position: "0,0,-1"
-
-  def shift_bodies(vector)
-    world_bodies.each do |body|
-      body.p = CP::Vec2.new(body.p.x + vector.x,body.p.y + vector.y)
-    end
-  end
-
+  draw :tile_map, model: "metro::ui::tile_map", file: "level.tmx",
+    # BUG: https://github.com/burtlo/metro/issues/4
+    viewport: Metro::Units::RectangleBounds.new(left: 0, right: 800, top: 0, bottom: 600)
 
   def space
     @space ||= begin
@@ -32,12 +26,17 @@ class FirstScene < GameScene
   end
 
   def show
-    map.viewport = viewport
-    map.show
+    # TODO: this positoning business needs to be sorted out. When the map specifies a start
+    # position for the player we should use that to center everything.
+    # viewport.shift Point.at(tile_map.map.properties["viewport.left"].to_i,0)
+    # viewport.shift Point.at(Game.center.x - hero.body.p.x,0)
 
-    layer = map.layers.first
 
-    object_group = map.map.object_groups.first
+    tile_map.show
+
+    layer = tile_map.layers.first
+
+    object_group = tile_map.map.object_groups.first
     floors = object_group.objects.find_all {|object| object.type == "floor" }
 
     floors.each do |floor|
@@ -75,10 +74,6 @@ class FirstScene < GameScene
     @delta ||= (1.0/60.0)
   end
 
-  def viewport
-    @viewport ||= Game.bounds
-  end
-
   def update
     original_position = hero.body.p.x
     # gravity on just the hero
@@ -87,8 +82,8 @@ class FirstScene < GameScene
       space.step(delta)
     end
 
-    distance = (hero.body.p.x - original_position).to_f
-    viewport.shift(Point.at(distance,0))
+    distance = (hero.body.p.x - original_position).to_i
+    tile_map.viewport.shift(Point.at(distance,0))
 
     hero.body.reset_forces
     space.rehash_static
@@ -99,11 +94,12 @@ class FirstScene < GameScene
       x_position = shape.bb.r - shape.bb.l
       y_position = shape.bb.b - shape.bb.t
 
-      position_at = Point.at(shape.bb.l - viewport.left,shape.bb.t - viewport.top)
+      position_at = Point.at(shape.bb.l - tile_map.viewport.left,shape.bb.t - tile_map.viewport.top)
 
-      dim = Dimensions.of(x_position,y_position)
-      border = hero.create "metro::ui::border", position: position_at, dimensions: dim
-      border.draw
+      # dim = Dimensions.of(x_position,y_position)
+      # # using the hero to create the borders is because I don't have a good way to do it in the scene object
+      # border = hero.create "metro::ui::border", position: position_at, dimensions: dim
+      # border.draw
     end
   end
 
